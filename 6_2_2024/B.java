9f97e79f58ef2433d4dddc68a2e1b47a8df77236
Pratik Godase <114148313+pratikgodase07@users.noreply.github.com>
2025-02-07 14:10:27 +0530

    Add files via upload

---
+++ 6_2_2024/B.java
@@ -0,0 +1,46 @@
+/*prog to print no from 1 to 10 using for loop
+package org.tka;
+
+public class B {
+	public static void main(String args[])
+	{
+		for(int i=44; i>=22;i--)
+		{
+			System.out.println(i);
+		}
+	}
+
+}
+
+*/
+
+
+/*prog using  while loop
+package org.tka;
+public class B{
+	public static void main(String args[])
+	{
+		int i=44;
+		while(i>=22){
+			System.out.println(i);
+			i--;
+			
+		}
+	}
+}
+
+
+/*prog using do while loop
+package org.tka;
+public class B{
+	public static void main(String[]args) {
+int i=44;
+
+do {
+	System.out.println(i);
+	i--;
+}
+while(i>=22);
+}
+}
+*/
